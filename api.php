ae387dd69ed0ccd12e5c99536b90cea7a3641c51
alancyyuan <AlanCYYuan@fih-foxconn.com>
2021-02-14 18:47:33 +0800

    理api

---
+++ api.php
@@ -0,0 +1,13 @@
+<?php
+
+function load_cc($url){
+    $cc=curl_init();    // 初始化curl函式
+    curl_setopt($cc,CURLOPT_RETURNTRANSFER,true);
+    curl_setopt($cc,CURLOPT_URL,$url);
+    $data=curl_exec($cc);
+    curl_close($cc);
+    return $data;
+}
+echo $content=load_cc("https://gis.taiwan.net.tw/XMLReleaseALL_public/activity_C_f.json");
+
+?>
